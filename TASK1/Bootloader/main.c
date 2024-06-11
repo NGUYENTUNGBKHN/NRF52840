bb368f46e802962cce2fcb167efe1d35697a3a3a
nguyen-thanh-tung <nguyen-thanh-tung@WE2296-00>
2024-06-11 19:04:18 +0900

    add makefile common

---
+++ TASK1/Bootloader/main.c
@@ -0,0 +1,26 @@
+/******************************************************************************/
+/*! @addtogroup Group2
+    @file       main.c
+    @brief      
+    @date       2024/06/10
+    @author     Development Dept at Tokyo
+    @par        Revision
+    $Id$
+    @par        Copyright (C)
+    Japan CashMachine Co, Limited. All rights reserved.
+******************************************************************************/
+
+#include <stdint.h>
+
+
+int main()
+{
+    while (1)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
+
+
