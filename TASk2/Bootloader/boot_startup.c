5c1ecd1427d16733e7cc2bc69cfb23a5ad12a3bb
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2024-10-17 17:36:23 +0900

    add uart

--- TASk2/Bootloader/boot_startup.c
+++ TASk2/Bootloader/boot_startup.c
@@ -15,6 +15,31 @@ extern uint32_t _ebss;
 
 void Reset_Handler()
 {
+    uint32_t *start;
+    uint32_t *end;
+    uint32_t *data;
+
+    start = (uint32_t*)&_sdata;
+    end = (uint32_t*)&_edata;
+    data = (uint32_t*)&_sidata;
+    /* settup initialized data */
+    while (start < end)
+    {
+        /* code */
+        *start++ = *data++;
+    }
+    // /* implement main function */
+    // start = &_sbss;
+    // end = &_ebss;
+
+    // /* setup bss data = 0 */
+    // while (start < end)
+    // {
+    //     /* code */
+    //     *start++ = 0;
+    // }
+    
+
     main();
 }
 
