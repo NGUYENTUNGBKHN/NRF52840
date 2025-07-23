1882d1932e4593fcc94d206d1fd8ea57c116e06c
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2025-07-23 18:01:16 +0900

    update nrf sdk

---
+++ BLE_Project/NRF_SDK/components/softdevice/s140/hex/main.d
@@ -0,0 +1,4 @@
+..\..\..\..\..\..\components\softdevice\s140\hex\main.o: ..\..\..\main.c
+..\..\..\..\..\..\components\softdevice\s140\hex\main.o: D:\keilc\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+..\..\..\..\..\..\components\softdevice\s140\hex\main.o: D:\keilc\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
+..\..\..\..\..\..\components\softdevice\s140\hex\main.o: D:\keilc\ARM\ARM_Compiler_5.06u7\Bin\..\include\string.h
